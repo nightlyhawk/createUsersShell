ced14ed606ffed4f9f8e43e9302defaa6f66b027
Senanyo Benwari <65190458+nightlyhawk@users.noreply.github.com>
2024-07-04 18:32:52 +0100

    missing flags

--- create_users.sh
+++ create_users.sh
@@ -5,7 +5,7 @@ function create_users(){
 	local file = "$1"
 	while read -r line; do
 		IFS=';' read -ra USER <<< "$line"
-		if grep "${USER[0]}" /etc/passwd; then
+		if grep -q "${USER[0]}" /etc/passwd; then
 			echo "User ${USER[0]} already exists. Skipping." >> "$logfile"
 			continue
 		else
@@ -14,16 +14,15 @@ function create_users(){
 			echo "${USER[0]}:$PASSWORD" | chpasswd
 			echo "User ${USER[0]} has been created with password: $PASSWORD" >> "$logfile"
 			echo "${USER[0]},$PASSWORD" >> "$passwdstore"
-			chmod 400 "$passwdstore"
 
 		fi
 
 		IFS=',' read -ra GROUPS <<< "${USER[1]}"
-		for GROUP in "${GROUPS[@]}":
+		for GROUP in "${GROUPS[@]}"; do
 			if id -nG "${USER[0]}" | grep -qw "$GROUP"; then
 				echo "User ${USER[0]} is already in group $GROUP. Skipping." >> "$logfile"
 				continue
-			elif grep -w "$GROUP" /etc/group; then
+			elif grep -qw "$GROUP" /etc/group; then
 				usermod -a -G "$GROUP" "${USER[0]}"
 				echo "User ${USER[0]} added to existing group $GROUP." >> "$logfile"
 			else
@@ -31,9 +30,10 @@ function create_users(){
 				usermod -a -G "$GROUP" "${USER[0]}"
 				echo "Group $GROUP created and user ${USER[0]} added to it." >> "$logfile"
 			fi
-	
+		done
 
 	done <$file
+ 	chmod 400 "$passwdstore"
 }
 
 create_users "$1"
