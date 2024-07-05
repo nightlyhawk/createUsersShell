9cfb812c69616814c9d19938d53e66bd6665e772
Senanyo Benwari <65190458+nightlyhawk@users.noreply.github.com>
2024-07-05 23:47:47 +0100

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -18,8 +18,7 @@ function create_users(){
 		fi
 
 		IFS=',' read -r -a groups <<< "${USER[1]}"
-		for group in "${groups[@]}"; do
-  			GROUP=group | tr -d '[:blank:]'
+		for GROUP in "${groups[@]}"; do
 			if id -nG "${USER[0]}" | grep -qw "$GROUP"; then
 				echo "User ${USER[0]} is already in group $GROUP. Skipping." >> "$logfile"
 				continue
